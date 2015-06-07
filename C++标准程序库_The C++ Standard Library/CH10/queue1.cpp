ff8d15a142ae22cad7e18cfe8b979939719fc783
sexybear <wuwenxiaoi4@gmail.com>
2015-06-07 20:59:18 +0800

    reading note

---
+++ C++标准程序库_The C++ Standard Library/CH10/queue1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  queue<string> q;
+  q.push("these");
+  q.push("are");
+  q.push("more than");
+
+  cout << q.front();
+  q.pop();
+  cout << q.front();
+  q.pop();
+
+
+  q.push("four ");
+  q.push("words ");
+
+  q.pop();
+
+  cout << q.front();
+  q.pop();
+  cout << q.front() << endl;
+  q.pop();
+
+  cout << "number of elements in the queue: "<< q.size() << endl;
+}
