496d47b32e613727a616972ce502c6747601b2bc
sexybear <wuwenxiaoi4@gmail.com>
2015-06-07 20:04:49 +0800

    reading note

---
+++ C++标准程序库_The C++ Standard Library/CH10/stack1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+  stack<int> st;
+
+  st.push(1);
+  st.push(2);
+  st.push(3);
+
+  cout << st.top() << " ";
+  st.pop();
+
+  cout << st.top() << " ";
+  st.pop();
+
+  st.top() = 77;
+
+  st.push(4);
+  st.push(5);
+
+  st.pop();
+  while(!st.top())
+  {
+    cout << st.top() <<" ";
+    st.pop();
+  }
+  cout<< endl;
+}
